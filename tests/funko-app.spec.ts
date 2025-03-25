2ea39bbd112fdc173591b24ddeaed64d014bb070
Marlon Salazar <alu0101433943@ull.edu.es>
2025-03-25 21:53:03 +0000

    Mejorando coverage

--- tests/funko-app.spec.ts
+++ tests/funko-app.spec.ts
@@ -3,6 +3,7 @@ import { addFunko, listFunkos, getUserFolder } from "../src/funko-app";
 import { FunkoType, FunkoGenre } from "../src/models/funko";
 import { readFunko } from "../src/commands/read";
 import { updateFunko } from "../src/commands/update";
+import { deleteFunko } from "../src/commands/remove";
 import fs from "fs";
 import chalk from "chalk";
 
@@ -132,5 +133,115 @@ describe("addFunko", () => {
     readFunko({ id: 1, user: "testUser" });
     const allConsoleOutput = consoleSpy.mock.calls.flat().join(" ");
     expect(allConsoleOutput).toContain("Nuevo nombre");
-  }); 
+  });
+
+  test("Debe mostrar error al intentar modificar un funko introduciendo un ID que no se corresponde a un funko de la colección", () => {
+    updateFunko({
+      id: 1,
+      user: "testUser",
+      name: "Nuevo nombre",
+      description: funko2.description,
+      type: funko2.type,
+      genre: funko2.genre,
+      franchise: funko2.franchise,
+      number: funko2.number,
+      exclusive: funko2.exclusive,
+      specialFeatures: funko2.specialFeatures,
+      marketValue: funko2.marketValue,
+    });
+    expect(consoleSpy).toHaveBeenCalledWith(
+      chalk.red(
+        "Error: No se encontró un Funko con ID 1 en la lista de testUser.",
+      ),
+    );
+  });
+
+  test("Debe mostrar error al intentar modificar un funko con un género inválido", () => {
+    addFunko(funko2);
+    updateFunko({
+      id: 1,
+      user: "testUser",
+      name: "Nuevo nombre",
+      description: funko2.description,
+      type: funko2.type,
+      genre: "Género inválido",
+      franchise: funko2.franchise,
+      number: funko2.number,
+      exclusive: funko2.exclusive,
+      specialFeatures: funko2.specialFeatures,
+      marketValue: funko2.marketValue,
+    });
+    expect(consoleSpy).toHaveBeenCalledWith(
+      chalk.red(`Género de Funko inválido: Género inválido`),
+    );
+  });
+
+  test("Debe mostrar error al intentar modificar un funko con un tipo inválido", () => {
+    addFunko(funko2);
+    updateFunko({
+      id: 1,
+      user: "testUser",
+      name: "Nuevo nombre",
+      description: funko2.description,
+      type: "Tipo inválido",
+      genre: funko2.genre,
+      franchise: funko2.franchise,
+      number: funko2.number,
+      exclusive: funko2.exclusive,
+      specialFeatures: funko2.specialFeatures,
+      marketValue: funko2.marketValue,
+    });
+    expect(consoleSpy).toHaveBeenCalledWith(
+      chalk.red(`Tipo de Funko inválido: Tipo inválido`),
+    );
+  });
+
+  test("Debe borrar el funko previamente añadido", () => {
+    addFunko(funko2);
+    updateFunko({
+      id: 1,
+      user: "testUser",
+      name: "Nuevo nombre",
+      description: funko2.description,
+      type: funko2.type,
+      genre: funko2.genre,
+      franchise: "SA",
+      number: 2000,
+      exclusive: funko2.exclusive,
+      specialFeatures: funko2.specialFeatures,
+      marketValue: 1000000,
+    });
+    readFunko({ id: 1, user: "testUser" });
+    const allConsoleOutput = consoleSpy.mock.calls.flat().join(" ");
+    expect(allConsoleOutput).toContain("Nuevo nombre");
+    deleteFunko({ user: "testUser", id: 1 });
+    consoleSpy.mockRestore();
+    readFunko({ id: 1, user: "testUser" });
+    const allConsoleOutput2 = consoleSpy.mock.calls.flat().join(" ");
+    expect(allConsoleOutput2).not.toContain("1");
+  });
+
+  test("Debe mostrar error al intentar eliminar un funko en una colección vacía", () => {
+    deleteFunko({ user: "testUser", id: 1 });
+    expect(consoleSpy).toHaveBeenCalledWith(
+      chalk.red('Error: El usuario "testUser" no tiene Funkos almacenados.'),
+    );
+  });
+
+  test("Debe mostrar error al intentar eliminar un funko con un ID negativo", () => {
+    deleteFunko({ user: "testUser", id: -1 });
+    expect(consoleSpy).toHaveBeenCalledWith(
+      chalk.red("Error: El ID del Funko debe ser un número positivo."),
+    );
+  });
+
+  test("Debe mostrar error al intentar eliminar un funko con un ID que no existe en una colección que no está vacía", () => {
+    addFunko(funko2);
+    deleteFunko({ user: "testUser", id: 2 });
+    expect(consoleSpy).toHaveBeenCalledWith(
+      chalk.red(
+        "Error: No se encontró un Funko con ID 2 en la colección de testUser.",
+      ),
+    );
+  });
 });
